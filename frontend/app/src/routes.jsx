e3d9276ca06af26df13b1ebbad2971c672a9293c
NoahOrta22 <noahraulscout@gmail.com>
2024-01-21 14:18:55 -0600

    route name changes

--- frontend/app/src/routes.jsx
+++ frontend/app/src/routes.jsx
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-// import React from "react";
->>>>>>> f2bd9bd65ecc134a74c2614e19ba0bce2be76c37
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { AuthProvider } from "./context/auth";
 
@@ -9,12 +5,8 @@ import { LandingPage } from "./components/LandingPage";
 import { RegisterPage } from "./components/RegisterPage";
 import { LoginPage } from "./components/LoginPage";
 import { AboutPage } from "./components/AboutPage";
-<<<<<<< HEAD
 import { CoursePage } from "./components/CoursePage/CoursePage";
-=======
-import { CoursePage } from "./components/coursePage/coursePage";
->>>>>>> f2bd9bd65ecc134a74c2614e19ba0bce2be76c37
-import { SubmissionPage } from "./components/submissionPage/submissionPage";
+import { SubmissionPage } from "./components/SubmissionPage/SubmissionPage";
 
 export const AppRoutes = () => {
   return (
