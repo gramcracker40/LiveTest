4941da7c923d799038c73e751039d3866e58fac2
NoahOrta22 <noahraulscout@gmail.com>
2024-01-21 19:48:26 -0600

    added logout button

---
+++ frontend/app/src/components/logoutButton.jsx
@@ -0,0 +1,29 @@
+import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../context/auth.jsx';
+
+export const LogoutButton = () => {
+
+  const { updateAuthDetails } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    // Reset authDetails to default state
+    updateAuthDetails({
+      accessToken: "",
+      isLoggedIn: false,
+      type: "",
+      id: null,
+      email: "",
+      name: ""
+    });
+    // Navigate to the login page
+    navigate("/login");
+  };
+
+  return (
+      <button onClick={handleLogout} className="logout-button-styles">
+        Logout
+      </button>
+  );
+};
