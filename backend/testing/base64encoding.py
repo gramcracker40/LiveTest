1f067516c136dc31770e0ed1d78315d372282a51
Garrett Mathers <garrett.mathers@gmail.com>
2024-01-26 18:48:15 -0600

    fixed return values for noah

--- backend/testing/base64encoding.py
+++ backend/testing/base64encoding.py
@@ -1,7 +1,7 @@
 import base64
 
 
-test_file = open(f"../../test_data/FakeTest1/IMG_4162.jpg", 'rb').read()
+test_file = open(f"../../test_data/FakeTest1/IMG_4163.jpg", 'rb').read()
 test_base64_str = base64.b64encode(test_file).decode('utf8')
 
 ex_b64_str = open(f"b64_img.txt", 'w').write(test_base64_str)
