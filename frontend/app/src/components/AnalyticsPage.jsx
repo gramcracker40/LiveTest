9080ac270f1073e72e1eadeba7f40190e2d7f465
NoahOrta22 <noahraulscout@gmail.com>
2024-03-05 17:07:25 -0600

    Adding comments to Analytics and setting up the style for the page

--- frontend/app/src/components/AnalyticsPage.jsx
+++ frontend/app/src/components/AnalyticsPage.jsx
@@ -9,9 +9,23 @@ export const AnalyticsPage = () => {
 
     const test = location.state.test
 
+    const testAvg = 90;
+    const myGrade = 70;
+    const testHigh = 100;
+    const testLow = 50;
+
+    // I want to make a 2 column grid. on the left side will be a histogram of the grades of the whole class.
+    // and on the right side will be the standard deviation showing the high, low, avg, and students grade
+
     return (
-        <div>
-            Your test: {JSON.stringify(test)}
+        <div className=" min-h-screen mx-auto w-full bg-cyan-50">
+            <div className='sm:px-28 sm:py-8 px-4 py-4'>
+                <div className='gap-x-8 mb-7 p-4 rounded-lg shadow bg-white'>
+                    <h1 className='text-5xl justify-center flex'>
+                        {test.name}
+                    </h1>
+                </div>
+            </div>
         </div>
     )
 }
